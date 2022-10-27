e40eb5749169ad052985166b871fa28231ff6aed
BT_3W2_26 <timurb2@nycstudents.net>
2022-10-27 09:31:22 -0400

    gd

--- src/RandomTesterRunner.java
+++ src/RandomTesterRunner.java
@@ -1,7 +1,7 @@
 public class RandomTesterRunner {
     public static void main( String[] args){
         for (int i = 0; i < 100; i++) {
-            int randomNum = (int) (Math.random() * 36) + 50;/* put your code here to generate a random number between 50 and 85 */
+            int randomNum = (int) (Math.random() * 66) -15 ;/* put your code here to generate a random number between 50 and 85 */
                     System.out.println(randomNum);
         }
 
